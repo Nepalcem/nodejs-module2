b632dfda4dd11cb1b168c87194a525a619ae59da
Nepalcem <mark.emailtest@gmail.com>
2023-08-12 21:41:37 +0300

    Jimp & Write

--- controllers/authorizeController.js
+++ controllers/authorizeController.js
@@ -3,8 +3,9 @@ const bcrypt = require("bcryptjs");
 const gravatar = require("gravatar");
 const signToken = require("../utils/signToken");
 const path = require("path");
-const fs = require("fs/promises");
+
 const tryCatchHandler = require("../utils/tryCatchHandler");
+const Jimp = require("jimp");;
 
 exports.registrationController = tryCatchHandler(async (req, res) => {
   const { email, password } = req.body;
@@ -72,15 +73,37 @@ exports.updateSubscription = tryCatchHandler(async (req, res) => {
   return res.status(200).json({ message: "Subscription updated successfully" });
 });
 
+
+// exports.updateAvatar = tryCatchHandler(async (req, res) => {
+//   const { _id } = req.user;
+
+//   const { path: tempUpload, originalname } = req.file;
+//   const avatarDir = path.join(__dirname, "../", "public", "avatars");
+
+//   const filename = `${_id}_${originalname}`;
+//   const resultUpload = path.join(avatarDir, filename);
+
+//   await fs.rename(tempUpload, resultUpload);
+//   const avatarURL = path.join("avatars", filename);
+//   await User.findByIdAndUpdate(_id, { avatarURL });
+
+//   res.json({
+//     message: "Avatar changed successfully",
+//     avatarURL,
+//   });
+// });
+
 exports.updateAvatar = tryCatchHandler(async (req, res) => {
   const { _id } = req.user;
+
   const { path: tempUpload, originalname } = req.file;
   const avatarDir = path.join(__dirname, "../", "public", "avatars");
-
   const filename = `${_id}_${originalname}`;
-  const resultUpload = path.join(avatarDir, filename);
 
-  await fs.rename(tempUpload, resultUpload);
+  await Jimp.read(tempUpload).then((image) => {
+    return image.resize(250, 250).quality(80).write(path.join(avatarDir, filename));
+  })
+
   const avatarURL = path.join("avatars", filename);
   await User.findByIdAndUpdate(_id, { avatarURL });
 
